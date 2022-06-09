a3bd326d27ea210e9f77647272230ff5921cab7b
MBittencourt <home@iMac-de-Usuario.local>
2022-06-09 18:46:16 -0300

    Adding invoice attributes and genarating PDF

--- src/main/java/invoice/controller/InvoceController.java
+++ src/main/java/invoice/controller/InvoceController.java
@@ -8,9 +8,9 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import invoice.model.Invoice;
@@ -23,8 +23,8 @@ public class InvoceController {
    
 	
 	
-	@RequestMapping(value = "/teste", method = RequestMethod.POST, produces = "application/pdf")
-	public static ResponseEntity<InputStreamResource> teste(Model model, @RequestBody Invoice invoice) throws IOException {
+	@PostMapping("/teste")
+	public static ResponseEntity<?> teste(Model model, @RequestBody Invoice invoice) throws IOException {
 		
 		InvoiceService.generatePDF(invoice);
 		File pdfFile = new File("/Users/home/teste.pdf");
@@ -35,6 +35,7 @@ public class InvoceController {
 	            .contentLength(pdfFile.length())
 	            .contentType(MediaType.parseMediaType("application/pdf"))
 	            .body(new InputStreamResource(new FileInputStream(pdfFile)));
+//	            .body(InvoiceService.parseThymeleafTemplate(invoice));
 	}
 	
 
--- src/main/java/invoice/service/InvoiceService.java
+++ src/main/java/invoice/service/InvoiceService.java
@@ -1,7 +1,10 @@
 package invoice.service;
 
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -19,16 +22,20 @@ public class InvoiceService {
     private static String templatesPath = "/Users/home/Projetos/InvoiceGeneratorAPI/src/main/resources/templates/";
 
     public static void generatePDF(Invoice invoice) throws FileNotFoundException, IOException {
-        
 
-        parseThymeleafTemplate(invoice);
-        
+        String result = parseThymeleafTemplate(invoice);
         try (
-             OutputStream os = new FileOutputStream("/Users/home/teste.pdf")) {
+             OutputStream os = new FileOutputStream("/Users/home/teste.pdf");) {
+
+            File file = new File("/Users/home/teste.html");
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+
+            bw.write(result);
+            bw.close();
 
             PdfRendererBuilder builder = new PdfRendererBuilder();
             builder.useFastMode();
-            builder.withUri("file://" + templatesPath +  "template.html");
+            builder.withUri("file:///Users/home/teste.html");
             builder.toStream(os);
             builder.run();
 
@@ -36,7 +43,7 @@ public class InvoiceService {
 
     }
 
-    private static String parseThymeleafTemplate(Invoice invoice) {
+    public static String parseThymeleafTemplate(Invoice invoice) {
         ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
         templateResolver.setSuffix(".html");
         templateResolver.setTemplateMode(TemplateMode.HTML);
@@ -45,7 +52,7 @@ public class InvoiceService {
         templateEngine.setTemplateResolver(templateResolver);
 
         Context context = new Context();
-        context.setVariable("invoice", invoice.getDestinataryName());
+        context.setVariable("invoice", invoice);
 
         return templateEngine.process("templates/template", context);
     }
