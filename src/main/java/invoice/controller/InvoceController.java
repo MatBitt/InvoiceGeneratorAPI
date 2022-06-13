680dd6712a5a5b636fd7cef3e013194149d7abc0
home <home@192.168.1.109>
2022-06-13 12:30:23 -0300

    Response json already formatted and excluded controller responsibilities

--- src/main/java/invoice/controller/InvoceController.java
+++ src/main/java/invoice/controller/InvoceController.java
@@ -3,8 +3,6 @@ package invoice.controller;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.validation.Valid;
 
@@ -12,7 +10,6 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import invoice.exception.BadRequestExceptionHandler;
 import invoice.model.Invoice;
 import invoice.service.InvoiceService;
 import invoice.utils.Path;
@@ -41,19 +39,16 @@ public class InvoceController {
                 .contentType(MediaType.parseMediaType("application/pdf"))
                 .body(new InputStreamResource(new FileInputStream(pdfFile)));
     }
-    
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {        
-        Map<String, String> errors = new HashMap<>();
-        errors.put("Status", HttpStatus.BAD_REQUEST.toString());
-        
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
+    public ResponseEntity<BadRequestExceptionHandler> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        BadRequestExceptionHandler response = new BadRequestExceptionHandler(ex);
+
+        return ResponseEntity
+                .ok()
+                .body(response);
     }
 
 }
